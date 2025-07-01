04181c979aaa39c8af2c692680331cfe5ccef1c8
dragonflyz369 <jessiecahoon@gmail.com>
2025-06-30 21:22:03 -0400

    Fix back - wasnt working

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,12 @@
 import type { NextConfig } from "next";
 import withFlowbiteReact from "flowbite-react/plugin/nextjs";
 
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig: NextConfig = {
   /* config options here */
-  basePath: '/Portfolio',
-  assetPrefix: '/Portfolio/',
+  basePath: isProd ? '/Portfolio' : '',
+  assetPrefix: isProd ? '/Portfolio' : '',
   images: {
     dangerouslyAllowSVG: true
   }
