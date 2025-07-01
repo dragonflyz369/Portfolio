db2396d948af71e36046bb63e88b178fb374527c
dragonflyz369 <jessiecahoon@gmail.com>
2025-06-30 21:06:36 -0400

    Updates to set references for images

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,12 @@
 import type { NextConfig } from "next";
 import withFlowbiteReact from "flowbite-react/plugin/nextjs";
 
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig: NextConfig = {
   /* config options here */
+  basePath: isProd ? '/portfolio' : '',
+  assetPrefix: isProd ? '/portfolio/' : '',
 };
 
 export default withFlowbiteReact(nextConfig);
